f15eb0f399acfafac6f6a0d94835526f51fc6bb8
zeidlitz <zeidlitzerik@gmail.com>
2024-09-10 23:59:01 +0200

    Formatting

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,15 @@
 package main
 
-import(
-  "fmt"
-  "github.com/zeidlitz/go-balancer/internal/env"
-  "github.com/zeidlitz/go-balancer/internal/server"
+import (
+	"fmt"
+	"github.com/zeidlitz/go-balancer/internal/env"
+	"github.com/zeidlitz/go-balancer/internal/server"
 )
 
-func main()  {
-  serverList := env.GetString("SERVERS","server1, server2")
-  host := env.GetString("HOST","localhost")
-  port := env.GetString("PORT","8080")
-  host = fmt.Sprint(host + ":" + port)
-  server.Start(host)
+func main() {
+	serverList := env.GetString("SERVERS", "server1, server2")
+	host := env.GetString("HOST", "localhost")
+	port := env.GetString("PORT", "8080")
+	host = fmt.Sprint(host + ":" + port)
+	server.Start(host)
 }
